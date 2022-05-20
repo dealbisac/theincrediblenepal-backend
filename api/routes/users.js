1e387b00dc4f572aa4f5bd0a66c890eb8f457b2b
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-05-21 01:11:51 +0545

    Feature: add user api

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,18 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/UserController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello, This is the users endpoint");
-});
+//UPDATE
+router.put("/:id",updateUser);
 
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET SINGLE USER (READ)
+router.get("/:id", getUser);
+
+//GET ALL USERS(READ ALL)
+router.get("/", getUsers);
+ 
 export default router;
