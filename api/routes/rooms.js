add0614313a3264cb8180fee1691eca86da6a322
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-05-24 00:11:46 +0545

    Feature: add room model and controller

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,23 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello, This is the rooms endpoint");
-});
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+//GET SINGLE ROOM (READ)
+router.get("/:id", getRoom);
+
+//GET ALL ROOMS(READ ALL)
+router.get("/", getRooms);
+
 
 export default router;
